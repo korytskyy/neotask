9f060b91fc302157f7df9f7ec432a415c3881663
korytskyy <sergiy.korytskyy@gmail.com>
2019-04-02 16:56:26 +0200

    add end to end test

---
+++ src/test/java/com/korytskyy/neotask/e2e/EndToEndTest.java
@@ -0,0 +1,43 @@
+package com.korytskyy.neotask.e2e;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+import javax.annotation.Resource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.http.MediaType.TEXT_PLAIN;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestPropertySource(locations = "classpath:test.properties")
+public class EndToEndTest {
+    @Resource
+    private WebTestClient client;
+    
+    @Value("${token.ttl}")
+    private int ttl;
+    
+    @Test
+    public void shouldCreateTokenAndForwardToProperUrl() throws InterruptedException {
+        String token = client.get().uri("/generateToken?url=http://www.google.com").exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(TEXT_PLAIN)
+                .expectBody(String.class).value(s -> assertThat(s).isNotBlank())
+                .returnResult().getResponseBody();
+
+        client.get().uri("/token/" + token).exchange()
+                .expectStatus().is3xxRedirection()
+                .expectHeader().valueEquals("Location", "http://www.google.com");
+        
+        Thread.sleep(ttl * 1000);
+    
+        client.get().uri("/token/" + token).exchange()
+                .expectStatus().isNotFound();
+    }
+}
