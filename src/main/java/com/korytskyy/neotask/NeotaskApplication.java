d7431bbe7e6dd078a4a0f11ae64ab9b2c8b324a7
korytskyy <sergiy.korytskyy@gmail.com>
2019-04-02 15:40:18 +0200

    initial spring skeleton

---
+++ src/main/java/com/korytskyy/neotask/NeotaskApplication.java
@@ -0,0 +1,13 @@
+package com.korytskyy.neotask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeotaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeotaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/korytskyy/neotask/NeotaskApplicationTests.java
@@ -0,0 +1,16 @@
+package com.korytskyy.neotask;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NeotaskApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
